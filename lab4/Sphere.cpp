9f8eb60cdf9bcb5297d2ba7c4b2fa808723efc13
Søren Hougaard Mulvad <shmulvad@gmail.com>
2019-10-24 15:41:22 +0800

    Add Lab4 files

---
+++ lab4/Sphere.cpp
@@ -0,0 +1,34 @@
+//============================================================
+// STUDENT NAME: 
+// STUDENT NO.: 
+// NUS EMAIL ADDRESS: 
+// COMMENTS TO GRADER: 
+// 
+// ============================================================
+
+#include <cmath>
+#include "Sphere.h"
+
+using namespace std;
+
+
+
+bool Sphere::hit( const Ray &r, double tmin, double tmax, SurfaceHitRecord &rec ) const 
+{
+    //***********************************************
+    //*********** WRITE YOUR CODE HERE **************
+    //***********************************************
+
+    return false; // You can remove/change this if needed.
+}
+
+
+
+bool Sphere::shadowHit( const Ray &r, double tmin, double tmax ) const 
+{
+    //***********************************************
+    //*********** WRITE YOUR CODE HERE **************
+    //***********************************************
+
+    return false; // You can remove/change this if needed.
+}
